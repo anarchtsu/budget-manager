eb35426bd08e31c6890c04321c8e739f68954e7e
anarchtsu <gontarenko.dev@gmail.com>
2022-03-17 19:23:10 +0300

    init

---
+++ services/web-gateway/src/main/java/ru/gontarenko/webgateway/WebGatewayApplication.java
@@ -0,0 +1,19 @@
+package ru.gontarenko.webgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients(
+        basePackages = "ru.gontarenko.feignclients"
+)
+@EnableEurekaClient
+@SpringBootApplication
+public class WebGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebGatewayApplication.class, args);
+    }
+
+}
